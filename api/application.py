2b332419b6c0c2221c83c17da49985181c966b2a
Mwai Banda <bandamwai@gmail.com>
2022-07-11 23:00:49 -0500

    Added books.db, getAllBooks, getBook

--- api/application.py
+++ api/application.py
@@ -1,6 +1,27 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+"""
+Book Model
+id
+book_name
+author
+publisher
+"""
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(100), unique=True)
+    author = db.Column(db.String(100))
+    publisher = db.Column(db.String(100))
+
+    def __repr__(self):
+        return f'{self.book_name} by {self.author}'
 
 
 @app.route('/')
@@ -9,5 +30,28 @@ def home():
 
 
 @app.route('/books')
-def books():
-    return {'books': 'are empty'}
+def getBooks():
+    books = Book.query.all()
+    result = []
+    for book in books:
+        book = {
+            'id': book.id,
+            'book_name': book.book_name,
+            'author': book.author,
+            'publisher': book.publisher
+        }
+        result.append(book)
+
+    return {'books': result}
+
+@app.route('/books/<id>')
+def getbook(id):
+    book = Book.query.get_or_404(id)
+    result = {
+            'id': book.id,
+            'book_name': book.book_name,
+            'author': book.author,
+            'publisher': book.publisher
+
+    }
+    return result
